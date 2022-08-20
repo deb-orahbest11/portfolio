824eaf733b9381bc535793aaf1f35c7c9306de94
deb-orahbest11 <smile204103@gmail.com>
2022-08-20 12:30:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
+import Header from './Components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import logo from './assets/logo.svg';
+import Profile from './pages/Profile';
+import Projects from './pages/Projects';
 import './App.css';
+import ProjectDetail from './pages/ProjectDetail';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header logo={logo} />
+        <Routes>
+          <Route path='/' element={<Profile userName='deb-orahbest11' />} />
+          <Route path='/projects' element=
+            {<Projects userName='deb-orahbest11' />} />
+          <Route path='/projects/:name' element=
+            {<ProjectDetail userName='deb-orahbest11' />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
